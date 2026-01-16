816484337a4fece295a356e0da281666dbd96d5c
Christian Weilbach <christian@weilbach.name>
2026-01-16 00:41:37 -0800

    Migrate to org.replikativ namespace
    - Update build.clj to publish as org.replikativ/persistent-sorted-set
    - Tests: 10 tests, 926 assertions, 0 failures

--- build.clj
+++ build.clj
@@ -6,7 +6,7 @@
   (:import [clojure.lang ExceptionInfo]))
 
 (def org "replikativ")
-(def lib 'io.replikativ/persistent-sorted-set)
+(def lib 'org.replikativ/persistent-sorted-set)
 (def current-commit (b/git-process {:git-args "rev-parse HEAD"}))
 (def version (format "0.3.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
